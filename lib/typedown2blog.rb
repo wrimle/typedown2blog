d75ad092d3197630e4bc985edb8687ab6139e640
Rune Myrland <rune@epubify.com>
2010-08-23 00:07:17 +0200

    Removing dead file

--- lib/typedown2blog.rb
+++ lib/typedown2blog.rb
@@ -2,6 +2,5 @@
 require 'typedown2blog/instance_exec.rb'
 require 'typedown2blog/base.rb'
 require 'typedown2blog/blog_post.rb'
-require 'typedown2blog/parse_mail.rb'
 require 'typedown2blog/formatters.rb'
 require 'typedown2blog/spec.rb'
