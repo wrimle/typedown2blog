45ff5af77248100c0867a1eeae4dcf2ee509e94d
Rune Myrland <rune@epubify.com>
2010-08-22 13:31:05 +0200

    Support yaml format for retriever settings

--- lib/typedown2blog/spec.rb
+++ lib/typedown2blog/spec.rb
@@ -3,6 +3,22 @@
 require 'mail_processor'
 
 module Typedown2Blog
+  def symbolize_keys(hash)
+    hash.inject({}){|result, (key, value)|  
+      new_key = case key  
+                when String then key.to_sym  
+                else key  
+                end  
+      new_value = case value  
+                  when Hash then symbolize_keys(value)  
+                  else value  
+                  end  
+      result[new_key] = new_value  
+      result  
+    }  
+  end
+
+
   class Spec
     def self.setup &block
       instance_eval &block if block_given?
@@ -11,7 +27,7 @@ module Typedown2Blog
 
     def self.retriever_method method, options={}, &block
       @retriever = MailProcessor::Processor.new do
-        retriever method, options, &block
+        retriever method, symbolize_keys(options), &block
       end
       @retriever
     end
@@ -26,7 +42,7 @@ module Typedown2Blog
 
     def self.delivery_method m, options= {}
       Mail.defaults do
-        delivery_method m, options
+        delivery_method m, symbolize_keys(options)
       end
     end
 
